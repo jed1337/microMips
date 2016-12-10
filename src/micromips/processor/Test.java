a2672637abeb3e8c060931efed6ca3ca0bd8b71d
jed1337 <j.caychingco@yahoo.com>
2016-12-10 16:43:35 +0800

    Finished regex overhaul (Untested)

--- src/micromips/processor/Test.java
+++ src/micromips/processor/Test.java
@@ -10,7 +10,12 @@ public class Test {
       Storage.storeDouble(0x3008, 0x8899AABBCCDDEEFFL);
       
       AssemblyRegex ar = new AssemblyRegex(
-         "label:  dsubu r1, r2, r3  ;Test"
+//         "label:  dsubu r1 , r2, r3  ;Test"
+//         "label:  ld r1, 1000(r23) ;Test"
+//           "nop"
+//           "daddiu r1, r2, 0x3"
+//           "beQc r1, r2, label"
+           "bc jump"
       );
    }
 }
--- src/utilities/AssemblyRegex.java
+++ src/utilities/AssemblyRegex.java
@@ -1,34 +1,23 @@
 package utilities;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AssemblyRegex {
-   private static final String LABEL     = "\\s*(\\w+:\\s+)";
-   private static final String WORD      = "(\\w+)";
-   private static final String OPCODE    = WORD+"\\s+";
-   private static final String REG       = "([rR]\\d{1,2})";
-   private static final String COMMA     = "\\s*,\\s*";
-   private static final String COMMENT   = "(\\s*;.*)?";
-   
-   private static final String OP = "\\s*\\(\\s*"; //open parenthesis
-   private static final String CP = "\\s*\\)\\s*"; //close parenthesis
-   
-   public static final Pattern R_TYPE     = Pattern.compile(LABEL+OPCODE+REG+COMMA+REG+COMMA+REG+COMMENT);
-   public static final Pattern LOAD_STORE = Pattern.compile(LABEL+OPCODE+REG+COMMA+WORD+OP+REG+CP+COMMENT);
-   public static final Pattern BNE        = Pattern.compile(LABEL+OPCODE+REG+COMMA+REG+COMMA+WORD+COMMENT);
-   public static final Pattern BC         = Pattern.compile(LABEL+OPCODE+WORD+COMMENT);
-   
-   public static final Pattern P_LABEL = Pattern.compile(LABEL);
-   public static final Pattern P_OPCODE = Pattern.compile(OPCODE);
-   public static final Pattern P_REG = Pattern.compile(REG);
+   private static final Pattern OP     = Pattern.compile("(\\s*\\(\\s*)"); //open parenthesis
+   private static final Pattern CP     = Pattern.compile("(\\s*\\)\\s*)"); //close parenthesis
+   private static final Pattern COMMA  = Pattern.compile("(,)");
+   private static final Pattern WORD   = Pattern.compile("(\\w+)");
+
+   private static final Pattern LABEL  = Pattern.compile("\\s*(\\w+:\\s+)");
+   private static final Pattern OPCODE = Pattern.compile("(\\w+)");
+   private static final Pattern REG    = Pattern.compile("([rR]\\d{1,2})");
    
    private String input;
    private String origInput;
    private Matcher matcher;
-   private final HashMap<INSTRUCTION_CONTENTS, String> iContents = new HashMap<>();
-   
+   private final EnumMap<INSTRUCTION_CONTENTS, String> iContents = new EnumMap<>(INSTRUCTION_CONTENTS.class);
    
    public AssemblyRegex(String input){
       this.input = input;
@@ -38,61 +27,92 @@ public class AssemblyRegex {
 
    private void test() {
       setAndRemoveComments();
-      setAndRemove(P_LABEL, INSTRUCTION_CONTENTS.LABEL);
-      setAndRemove(P_OPCODE, INSTRUCTION_CONTENTS.OPCODE);
+      setAndRemove(LABEL, INSTRUCTION_CONTENTS.LABEL, false);
+      setAndRemove(OPCODE, INSTRUCTION_CONTENTS.OPCODE);
       
       INSTRUCTION instruction = INSTRUCTION.getInstruction(iContents.get(INSTRUCTION_CONTENTS.OPCODE));
       if (instruction == null) {
-         System.out.println("INVALID");
+         System.err.println("INVALID");
          //add mips exception here
       }
       else{
+         System.out.println("Instruction is: "+instruction);
          switch(instruction){
             case XOR:
             case DSUBU:
             case SLT:
-               System.out.println("R TYPE");
-               setAndRemove(P_REG, INSTRUCTION_CONTENTS.RD);
-               setAndRemove(P_REG, INSTRUCTION_CONTENTS.RS);
-               setAndRemove(P_REG, INSTRUCTION_CONTENTS.RT);
+               setAndRemove(REG , INSTRUCTION_CONTENTS.RD);
+               remove(COMMA);
+               setAndRemove(REG , INSTRUCTION_CONTENTS.RS);
+               remove(COMMA);
+               setAndRemove(REG , INSTRUCTION_CONTENTS.RT);
                break;
             case NOP:
-               System.out.println("NOP");
                break;
             case LD:
             case SD:
-               
-               System.out.println("MEM");
+               setAndRemove(REG  , INSTRUCTION_CONTENTS.RT);
+               remove(COMMA);
+               setAndRemove(WORD , INSTRUCTION_CONTENTS.IMM);
+               remove(OP);
+               setAndRemove(REG  , INSTRUCTION_CONTENTS.RS);
+               remove(CP);
                break;
             case BEQC:
+               setAndRemove(REG , INSTRUCTION_CONTENTS.RS);
+               remove(COMMA);
+               setAndRemove(REG , INSTRUCTION_CONTENTS.RT);
+               remove(COMMA);
+               setAndRemove(WORD , INSTRUCTION_CONTENTS.IMM);
+               break;
             case DADDIU:
-               System.out.println("IMM");
+               setAndRemove(REG , INSTRUCTION_CONTENTS.RT);
+               remove(COMMA);
+               setAndRemove(REG , INSTRUCTION_CONTENTS.RS);
+               remove(COMMA);
+               setAndRemove(WORD , INSTRUCTION_CONTENTS.IMM);
                break;
             case BC:
-               System.out.println("JTYPE");
+               setAndRemove(WORD, INSTRUCTION_CONTENTS.IMM);
                break;
          }
          //Add mips exception if input still has contents
       }
-      
-      System.out.println(input);
+      if(!input.equals("")){
+         System.err.println("Extraeneous input: "+input);
+      }
    }
    
    public void setAndRemoveComments(){
       int sIndex = this.input.indexOf(';');
-      iContents.put(INSTRUCTION_CONTENTS.COMMENT, this.input.substring(sIndex));
       if(sIndex != -1){
+         iContents.put(INSTRUCTION_CONTENTS.COMMENT, this.input.substring(sIndex));
          this.input = this.input.substring(0, sIndex).trim();
       }
    }
    
+   private void remove(Pattern pattern){
+      setAndRemove(pattern, null);
+   }
    private void setAndRemove(Pattern pattern, INSTRUCTION_CONTENTS content) {
+      setAndRemove(pattern, content, true);
+   }
+   private void setAndRemove(Pattern pattern, INSTRUCTION_CONTENTS content, boolean required) {
       matcher = pattern.matcher(this.input);
       if(matcher.find()){
-         iContents.put(content, matcher.group(1).trim());
+         if(matcher.start(1)!=0){
+            //add mips error for has extraeneous input somewhere
+            System.err.println("Extraeneous input: "
+               +this.input.substring(0, matcher.start(1)));
+         }
+         if(content != null){ //From the remove instruction. Most likely violates OOP
+            iContents.put(content, matcher.group(1).trim());
+         }
          this.input = this.input.substring(matcher.end(1));
+      }else if(required){
+         //add mips error for missing item
+         System.err.println("You missed "+pattern);
       }
       this.input = input.trim();
    }
-
 }
