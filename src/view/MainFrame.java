f592b517579ab8c7a63660edafef0c196b257920
HoshijiroS <lynettedanielle.chan@gmail.com>
2016-12-12 18:08:34 +0800

    UI Updates

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -381,14 +381,6 @@ public class MainFrame extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_loadButtonMouseClicked
 
-    /**
-     * @param args the command line arguments
-     */
-    
-    public String getMemoryAddress() {
-        return this.memoryAddress;
-    }
-    
     public void setValue(String memoryAddress, String value, int toggle, int row) {
         String s;
     
@@ -412,6 +404,10 @@ public class MainFrame extends javax.swing.JFrame {
         }
     }
 
+    /**
+     * @param args the command line arguments
+     */
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -425,22 +421,16 @@ public class MainFrame extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        
+        //</editor-fold>
       
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainFrame().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new MainFrame().setVisible(true);
         });
     }
 
@@ -471,5 +461,4 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JTable registerTable;
     private javax.swing.JTextArea sourceCodeArea;
     // End of variables declaration//GEN-END:variables
-    private String memoryAddress;
 }
